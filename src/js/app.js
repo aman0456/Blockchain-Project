18a455f2c381276072b432ce4a0ff9b2f3ccfbe0
Aman Bansal <aman005bansal003@gmail.com>
2019-10-12 19:43:05 +0530

    Updated app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,133 +1,190 @@
 App = {
-  web3Provider: null,
-  contracts: {},
-  account: '0x0',
-  hasVoted: false,
+	web3Provider: null,
+	contracts: {},
+	account: '0x0',
+	hasVoted: false,
 
-  init: function() {
-    return App.initWeb3();
-  },
+	init: function() {
+		return App.initWeb3();
+	},
 
-  initWeb3: function() {
-    // TODO: refactor conditional
-    if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
-    } else {
-      // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-      web3 = new Web3(App.web3Provider);
-    }
-    return App.initContract();
-  },
+	initWeb3: function() {
+		// TODO: refactor conditional
+		if (typeof web3 !== 'undefined') {
+			// If a web3 instance is already provided by Meta Mask.
+			App.web3Provider = web3.currentProvider;
+			web3 = new Web3(web3.currentProvider);
+		} else {
+			// Specify default instance if no web3 instance provided
+			App.web3Provider = new Web3.providers.HttpProvider('http://10.42.0.139:7545');
+			web3 = new Web3(App.web3Provider);
+		}
+		return App.initContract();
+	},
 
-  initContract: function() {
-    $.getJSON("Network.json", function(network) {
-      // Instantiate a new truffle contract from the artifact
-      App.contracts.Network = TruffleContract(network);
-      // Connect provider to interact with contract
-      App.contracts.Network.setProvider(App.web3Provider);
+	initContract: function() {
+		$.getJSON("Network.json", function(network) {
+			// Instantiate a new truffle contract from the artifact
+			App.contracts.Network = TruffleContract(network);
+			// Connect provider to interact with contract
+			App.contracts.Network.setProvider(App.web3Provider);
+			web3.eth.defaultAccount = web3.eth.accounts[0]
+			web3.eth.getCoinbase(function(err, account) {
+				if (err === null) {
+					if (account == null) {
+						console.log('getting null account');
+						App.account = 1;
+					}
+					else {
+						App.account = account;
+					}
+					$("#accountAddress").html("Your Account: " + account);
+				}
+			});
+			// web3.personal.unlockAccount(web3.eth.defaultAccount, "", 0)
+			window.ethereum.enable().then(function(instance) {
+				console.log(instance);
+				console.log("defaultAccount = " + web3.eth.defaultAccount)
+				App.listenForEvents();
+				App.contracts.Network.deployed().then(function(instance) {
+					 networkInstance = instance;
+					 return networkInstance.existUser({ from: App.account});
+					 // return networkInstance.addUser("temp1", "temp1", { from: App.account});   //TODO: Make this function
+				}).then(function(doesExist) {
+					if (!doesExist) {
+						networkInstance.addUser("temp1", "temp1", { from: App.account});   //TODO: Make this function
+					}
+					return App.render();
+				}).catch(function(error) {
+					console.warn(error);
+				});
+				// $('#pointssTable').append("<tr> <td> Achieved this </td> <td> not verified </td> </tr>");
+			});
+		});
+	},
 
-      App.listenForEvents();
-      // $('#pointssTable').append("<tr> <td> Achieved this </td> <td> not verified </td> </tr>");
-    
-      return App.render();
-    });
-  },
+	// Listen for events emitted from the contract
+	listenForEvents: function() {
+		App.contracts.Network.deployed().then(function(instance) {
+			// Restart Chrome if you are unable to receive this event
+			// This is a known issue with Metamask
+			// https://github.com/MetaMask/metamask-extension/issues/2393
+			instance.addUserEvent({}, {
+				fromBlock: 0,
+				toBlock: 'latest'
+			}).watch(function(error, event) {
+				console.log("event triggered adduser", error, event)
+				// Reload when a new vote is recorded
+				// App.render();
+			});
+			instance.addPointEvent({}, {
+				fromBlock: 0,
+				toBlock: 'latest'
+			}).watch(function(error, event) {
+				console.log("event triggered addpoint", event)
+				// Reload when a new vote is recorded
+				// App.render();
+			});
+			instance.deletePointEvent({}, {
+				fromBlock: 0,
+				toBlock: 'latest'
+			}).watch(function(error, event) {
+				console.log("event triggered delete point", event)
+				// Reload when a new vote is recorded
+				App.render();
+			});
+			instance.addVerifierEvent({}, {
+				fromBlock: 0,
+				toBlock: 'latest'
+			}).watch(function(error, event) {
+				console.log("event triggered add verifier", event)
+				// Reload when a new vote is recorded
+				App.render();
+			});
+			instance.respondPointEvent({}, {
+				fromBlock: 0,
+				toBlock: 'latest'
+			}).watch(function(error, event) {
+				console.log("event triggered respond point 		", event)
+				// Reload when a new vote is recorded
+				App.render();
+			});
+		});
+	},
 
-  // Listen for events emitted from the contract
-  listenForEvents: function() {
-    App.contracts.Network.deployed().then(function(instance) {
-      // Restart Chrome if you are unable to receive this event
-      // This is a known issue with Metamask
-      // https://github.com/MetaMask/metamask-extension/issues/2393
-      instance. votedEvent({}, {
-        fromBlock: 0,
-        toBlock: 'latest'
-      }).watch(function(error, event) {
-        console.log("event triggered", event)
-        // Reload when a new vote is recorded
-        App.render();
-      });
-    });
-  },
+	render: function() {
+		console.log("rendering")
+		var networkInstance;
+		var loader = $("#loader");
+		var content = $("#content");
+		loader.hide();
+		content.show();
 
-  render: function() {
-    var networkInstance;
-    var loader = $("#loader");
-    var content = $("#content");
+		// Load account data
+		console.log(web3.eth.accounts[0]);
+		console.log(web3.eth.accounts[1]);
+		console.log(web3.eth.accounts[2]);
+		App.contracts.Network.deployed().then(function(instance) {
+				 networkInstance = instance;
+				 return networkInstance.getPointsLength({ from: App.account});   //TODO: Make this function
+		}).then(function(userPointsCount) {
+			var userPoints = $("#pointsTable");
+			userPoints.empty();
+			console.log("displaying points");
+			console.log(userPointsCount)
+			for (var i = 0; i < userPointsCount; i++) {
+				console.log("hello");
+			} 
+			for (var i = 0; i < userPointsCount; i++) {
+				networkInstance.getPointByIndex(i, { from: App.account}).then(function(point) {
+					var pointEntry = "<tr> <td>" + point + "</td> <td> </td> </tr>";
+					userPoints.append(pointEntry);
+				});   //TODO: Make this function
+			}
+		}).catch(function(error) {
+			console.warn(error);
+		});
+		// get Points to verify
+		App.contracts.Network.deployed().then(function(instance) {
+				 networkInstance = instance;
+				 return networkInstance.getPendingVerificationsLength();   //TODO: Make this function
+		}).then(function(verifyPointsCount) {
+			var verityPoints = $("#verifyPointsTable");
+			verityPoints.empty();
 
-    loader.hide();
-    content.show();
-
-    // Load account data
-    web3.eth.getCoinbase(function(err, account) {
-      if (err === null) {
-        if (account == null) {
-          console.log('getting null account');
-          App.account = 1;
-        }
-        else {
-          App.account = account;
-        }
-        $("#accountAddress").html("Your Account: " + account);
-      }
-    });
-    // App.contracts.Network.deployed().then(function(instance) {
-    //      networkInstance = instance;
-    //      return networkInstance.getPointsCount();   //TODO: Make this function
-    // }).then(function(userPointsCount) {
-    //   var userPoints = $("#userPoints");
-    //   userPoints.empty();
-
-    //   for (var i = 1; i <= userPointsCount; i++) {
-    //     electionInstance.getPoint(i).then(function(point) {
-    //       userPoints.append(point);
-    //     })   //TODO: Make this function
-    //   }
-    //   loader.hide();
-    //   content.show();
-    // }).catch(function(error) {
-    //   console.warn(error);
-    // });
-    // Load contract data
-    App.contracts.Network.deployed().then(function(instance) {
-      networkInstance = instance;
-      console.log(networkInstance)
-      // return networkInstance.users(App.account);
-    }).then(function() {
-      var addedPoints = $("#pointsTable");
-      addedPoints.empty();
-
-      var toVerifyPoints = $('#verifyPointsTable');
-      toVerifyPoints.empty();
-      console.log(App.account);
-      networkInstance.users(App.account).then(function(user) {
-        console.log(user.points);
-      })
-      // var pointsCount = user.points;
-      // console.log(userPoints);
-      // console.log(pointsCount);
-    });
-  },
-
-  addPoint: function() {
-    var pointToBeAdded = $('#pointstr').val();
-    App.contracts.Network.deployed().then(function(instance) {
-      return instance.addPoint(pointToBeAdded, { from: 1 });
-    }).then(function(result) {
-      // Wait for votes to update
-      $("#content").hide();
-      $("#loader").show();
-    }).catch(function(err) {
-      console.error(err);
-    });
-  }
+			for (var i = 0; i < verifyPointsCount; i++) {
+				networkInstance.getPendingVerificationByIndex(i, { from: App.account}).then(function(point) {
+					var pointEntry = "<tr> <td>" + point + "</td> <td> <button type=\"submit\" class=\"btn btn-primary\" onclick=\"verify(" + i + "," + 1 + ")\">Verify</button></td> <button type=\"submit\" class=\"btn btn-primary\" onclick=\"verify(" + i + "," + 0 + ")\">Reject</button></td> </tr>"
+					verityPoints.append(point);
+				})   //TODO: Make this function
+			}
+		}).catch(function(error) {
+			console.warn(error);
+		});
+		loader.hide();
+		content.show();
+	},
+	//index, bool
+	//respondPoint
+	addPoint: function() {
+		console.log("adding point")
+		// console.log("defaultAccount = " + web3.eth.defaultAccount)
+		var pointToBeAdded = $('#pointstr').val();
+		App.contracts.Network.deployed().then(function(instance) {
+			return instance.addPoint(pointToBeAdded, { from: App.account, gas:3000000});
+		}).then(function(result) {
+			console.log("Added a point to " + App.account)
+			// $("#content").hide();
+			// $("#loader").show();
+			App.render()
+		}).catch(function(err) {
+			console.error(err);
+		});
+	}
 };
 
 $(function() {
-  $(window).load(function() {
-    App.init();
-  });
+	$(window).load(function() {
+		App.init();
+	});
 }); 
